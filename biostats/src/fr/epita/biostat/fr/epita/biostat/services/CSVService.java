597aeb9a41f76fefcf628c2b7490c0feb1e51baa
Présentation <tbr@tbr.com>
2025-09-30 12:17:58 +0200

    biostats deserialization

---
+++ biostats/src/fr/epita/biostat/fr/epita/biostat/services/CSVService.java
@@ -0,0 +1,43 @@
+package fr.epita.biostat.fr.epita.biostat.services;
+
+import fr.epita.biostat.datamodel.BioStatEntry;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CSVService {
+
+    public static List<BioStatEntry> readEntriesFromFile(String pathname) throws FileNotFoundException {
+        File file = new File(pathname);
+        Scanner sc = new Scanner(file);
+        List<BioStatEntry> entries = new ArrayList<>();
+        sc.nextLine(); // skip the first row, it is headers
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            line = line.replace("\"", "");
+            String[] columns = line.split(",");
+
+            BioStatEntry entry = new BioStatEntry(columns[0],
+                    columns[1],
+                    Integer.parseInt(columns[2].trim()),
+                    Integer.parseInt(columns[3].trim()),
+                    Integer.parseInt(columns[4].trim())
+            );
+            entries.add(entry);
+        }
+        sc.close();
+        return entries;
+    }
+
+    public static void writeEntriesFromFile(String pathname, List<BioStatEntry> entries) throws FileNotFoundException {
+        File file = new File(pathname);
+        PrintWriter pw = new PrintWriter(file);
+
+
+
+    }
+}
--- biostats/src/fr/epita/biostat/launcher/Launcher.java
+++ biostats/src/fr/epita/biostat/launcher/Launcher.java
@@ -1,6 +1,7 @@
 package fr.epita.biostat.launcher;
 
 import fr.epita.biostat.datamodel.BioStatEntry;
+import fr.epita.biostat.fr.epita.biostat.services.CSVService;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,26 +12,14 @@ import java.util.Scanner;
 public class Launcher {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("biostats/biostat.csv");
-        Scanner sc = new Scanner(file);
-        List<BioStatEntry> entries = new ArrayList<>();
-        sc.nextLine(); // skip the first row, it is headers
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            line = line.replace("\"", "");
-            String[] columns = line.split(",");
-
-            BioStatEntry entry = new BioStatEntry(columns[0],
-                    columns[1],
-                    Integer.parseInt(columns[2].trim()),
-                    Integer.parseInt(columns[3].trim()),
-                    Integer.parseInt(columns[4].trim())
-            );
-            entries.add(entry);
-        }
-
+        String pathname = "biostats/biostat.csv";
+        List<BioStatEntry> entries = CSVService.readEntriesFromFile(pathname);
         System.out.println(entries.size());
-        //I want a List<BioStatEntry>
-        sc.close();
+
+
     }
+
+
+
+
 }
