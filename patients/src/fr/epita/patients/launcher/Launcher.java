4dfc4fc422af9436e74df2bab10c9b2500e33482
Présentation <tbr@tbr.com>
2025-10-06 14:45:27 +0200

    refactor to be done in launcher

--- patients/src/fr/epita/patients/launcher/Launcher.java
+++ patients/src/fr/epita/patients/launcher/Launcher.java
@@ -8,9 +8,7 @@ import java.nio.file.Files;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Launcher {
 
@@ -26,10 +24,16 @@ public class Launcher {
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
             Date subscriptionDate = sdf.parse(parts[6]);
             Date now = new Date();
-
+            long rawSeniority = now.getTime() - subscriptionDate.getTime();
+            long conversionForOneYear = 1000l * 60 * 60 * 24 * 365;
+            double seniorityInYear = rawSeniority / conversionForOneYear;
+            System.out.println(seniorityInYear);
             Patient patient = new Patient(parts[0], parts[1],parts[2], parts[3], parts[4], Integer.parseInt(parts[5]), subscriptionDate);
             patients.add(patient);
         }
+
+        Map<String, Double> seniorityMap = new HashMap<String, Double>();
+
 
         //implement compute seniority
         System.out.println(patients.size());
